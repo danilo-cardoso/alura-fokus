c4b366d50495ab752cc975abda2ad62916353965
Nialods <105827271+Nialods@users.noreply.github.com>
2024-01-31 13:24:55 -0300

    criando two-way data binding para alterar background

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'alura-fokus';
-  currImage = '';
-  currText = '';
-  currStrong = '';
+
+  currBg = {
+    currImage: '../assets/Imagens/Imagem foco.png',
+    currText: 'Otimize sua produtividade,',
+    currStrong: 'mergulhe no que importa',
+    currColor: "url('../assets/Imagens/Background-linhas.png'), var(--linear-foco)"
+  }
+
+  constructor() { }
+
 }
--- src/app/components/timer/timer.component.ts
+++ src/app/components/timer/timer.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-timer',
@@ -7,4 +7,58 @@ import { Component } from '@angular/core';
 })
 export class TimerComponent {
 
+  @Input() currBg =  {
+    currImage: '../assets/Imagens/Imagem foco.png',
+    currText: 'Otimize sua produtividade,',
+    currStrong: 'mergulhe no que importa',
+    currColor: "url('../assets/Imagens/Background-linhas.png'), var(--linear-foco)"
+  };
+
+  @Output() currBgChange = new EventEmitter<{ currImage: string, currText: string, currStrong: string, currColor: string }>()
+
+  active = {
+    focus: 'active',
+    short: '',
+    long: ''
+  };
+
+  bgChange(input: string) {
+
+    switch (input) {
+      case 'focus':
+        this.currBg.currImage = '../assets/Imagens/Imagem foco.png';
+        this.currBg.currText = 'Otimize sua produtividade,';
+        this.currBg.currStrong = 'mergulhe no que importa';
+        this.currBg.currColor = "url('../assets/Imagens/Background-linhas.png'), var(--linear-foco)";
+        this.active.focus = 'active';
+        this.active.short = '';
+        this.active.long = '';
+        break;
+
+      case 'short':
+        this.currBg.currImage = '../assets/Imagens/Imagem descanso curto.png';
+        this.currBg.currText = 'Que tal dar uma respirada?';
+        this.currBg.currStrong = 'Faça uma pausa curta!';
+        this.currBg.currColor = "url('../assets/Imagens/Background-linhas.png'), var(--linear-curto)";
+        this.active.short = 'active';
+        this.active.focus = '';
+        this.active.long = '';
+        break;
+
+      case 'long':
+        this.currBg.currImage = '../assets/Imagens/Imagem descanso longo.png';
+        this.currBg.currText = 'Hora de voltar à superfície.';
+        this.currBg.currStrong = 'Faça uma pausa longa.';
+        this.currBg.currColor = "url('../assets/Imagens/Background-linhas.png'), var(--linear-longo)";
+        this.active.long = 'active';
+        this.active.short = '';
+        this.active.focus = '';
+        break;
+
+      default:
+        break;
+    }
+
+    this.currBgChange.emit(this.currBg);
+  }
 }
